6d336fc0dd311112aa18307a42632743fe4fbafc
markcalendario <markcalendario@gmail.com>
2025-05-14 11:52:27 +0800

    feat: add detext form

--- client/src/components/DetextForm/DetextForm.jsx
+++ client/src/components/DetextForm/DetextForm.jsx
@@ -2,6 +2,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Button } from "../Buttons/Buttons.jsx";
+import FraudDonut from "../FraudDonut/FraudDonut.jsx";
 import styles from "./DetextForm.module.scss";
 
 export default function DetextForm({ onSubmit, fraudConfidence }) {
@@ -25,6 +26,9 @@ export default function DetextForm({ onSubmit, fraudConfidence }) {
       </div>
       <div className={styles.result}>
         <h1 className={styles.title}>Analysis Result</h1>
+        <div className={styles.doughnutWrapper}>
+          <FraudDonut fraudConfidence={fraudConfidence || 0} />
+        </div>
       </div>
     </div>
   );
